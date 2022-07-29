230b8ec12ef3b69c145bf8e0906bd1ad1264acd1
Eo_Le_LearnToHack <eo.le.learntohack@gmail.com>
2022-07-29 10:51:35 +0200

    Add Stines solution

--- Tegneprogram/Program.cs
+++ Tegneprogram/Program.cs
@@ -13,7 +13,7 @@ int forskydningXpos = Firkant[0].Bredde / 16;         //1 enhed forskydning i X 
 int forskydningYpos = Firkant[0].Hojde / 12;          //1 enhed forskydning i Y position defineres her
 
 Cirkel[] Cirkel = new Cirkel[5]
-{new Cirkel("Blue"), 
+{new Cirkel("Blue"),
 new Cirkel("Yellow"),
 new Cirkel("Black"),
 new Cirkel("Green"),
@@ -23,9 +23,9 @@ for (int i = 0; i < Cirkel.Length; i++)
 {
     Cirkel[i].Diameter = 3 * forskydningXpos;                                                   //Diameter af cirklerne defineres her.
     Cirkel[i].X = Firkant[0].X + (i * 2 + 4) * forskydningXpos;                                 //Position X af alle 5 cirkler defineres her.
-    if      (i < 2)             { Cirkel[i].Y = Firkant[0].Y + (i * 2 + 5) * forskydningYpos; } //Position Y af Cirkel 1 og 2 defineres her.
-    else if (i == 2 || i == 4)  { Cirkel[i].Y = Cirkel[0].Y; }                                  //Position Y for Cirkel 3 og 5
-    else if (i == 3)            { Cirkel[i].Y = Cirkel[1].Y; }                                  //Position Y for Cirkel 4.
+    if (i < 2) { Cirkel[i].Y = Firkant[0].Y + (i * 2 + 5) * forskydningYpos; }                  //Position Y af Cirkel 1 og 2 defineres her.
+    else if (i == 2 || i == 4) { Cirkel[i].Y = Cirkel[0].Y; }                                   //Position Y for Cirkel 3 og 5
+    else if (i == 3) { Cirkel[i].Y = Cirkel[1].Y; }                                             //Position Y for Cirkel 4.
 }
 
 Tegning Tegning = new();
---
+++ Tegneprogram/Stine.cs
@@ -0,0 +1,48 @@
+﻿/* STINES MIDLERTIDIG LØSNING
+using Tegneprogram;
+using System;
+
+// Skriv dit program her.
+// God fornøjelse
+
+//string[] farve = { "Black", "Blue", "Yellow", "Green", "Red" };
+
+Firkant[] Firkant = new Firkant[1] { new Firkant("Black") };
+
+Firkant[0].Hojde = 300;
+Firkant[0].Bredde = 600;
+Firkant[0].X = 100;
+Firkant[0].Y = 100;
+
+
+Cirkel[] Cirkel = new Cirkel[5] { new Cirkel("Blue"), new Cirkel("Yellow"), new Cirkel("Black"), new Cirkel("Green"), new Cirkel("Red") };
+
+Cirkel[0].Diameter = 20;
+Cirkel[0].X = 10;
+Cirkel[0].Y = 60;
+
+Cirkel[1].Diameter = 20;
+Cirkel[1].X = 20;
+Cirkel[1].Y = 40;
+
+Cirkel[2].Diameter = 20;
+Cirkel[2].X = 35;
+Cirkel[2].Y = 60;
+
+Cirkel[3].Diameter = 20;
+Cirkel[3].X = 55;
+Cirkel[3].Y = 40;
+
+Cirkel[4].Diameter = 20;
+Cirkel[4].X = 60;
+Cirkel[4].Y = 60;
+
+//Herunder opretter jeg en instans til klassen Tegning:
+
+Tegning Tegning1 = new();
+
+//herunder kalder jeg metoden Tegn via klassen Tegning:
+//Metoden Tegn skal have to argumenter - firkant-array og cirkel-array
+
+Tegning1.Tegn(Firkant, Cirkel);
+*/
